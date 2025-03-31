ca435447a2ffbaa7a83b042cca135543347a77de
mariaJimenez <mjimgon@g.educaand.es>
2025-03-31 10:40:59 +0200

    script y check TERMINADOS

--- script_SENECA.sql
+++ script_SENECA.sql
@@ -9,7 +9,8 @@ create table persona (
     telefono char(9), 
     direccion varchar(120),
     email varchar(100), 
-    fecha_nac date
+    fecha_nac date,
+    constraint chk_email check (email like '%@%')
 );
 
 create table alumno (
@@ -29,7 +30,7 @@ create table profesor (
     salario decimal(6,2),
     persona char(9),
     constraint fk_profesor_persona foreign key (persona) references persona(dni),
-	constraint chk_profesor check (salario >= 0)
+	constraint chk_profesor check (salario > 0)
 );
 
 create table ciclo (
@@ -45,7 +46,9 @@ create table curso (
     fecha_fin date,
     centro varchar(100),
     ciclo char(6),
-    constraint fk_curso_ciclo foreign key (ciclo) references ciclo(codigo)
+    constraint fk_curso_ciclo foreign key (ciclo) references ciclo(codigo),
+	constraint chk_fecha check (fecha_inicio < fecha_fin),
+    constraint chk_num_max_alum check (num_max_alum > 10)
 );
 
 create table modulo_prof (
@@ -53,7 +56,8 @@ create table modulo_prof (
     nombre varchar(70),
     num_horas int,
     profesor char(9),
-    constraint fk_modulo_profesor foreign key (profesor) references profesor(dni_profesor)
+    constraint fk_modulo_profesor foreign key (profesor) references profesor(dni_profesor),
+	constraint chk_num_horas check (num_horas > 0)
 );
 
 create table matricula (
@@ -75,10 +79,50 @@ create table pertenece (
     constraint fk_pertenece_matricula foreign key (codigo_matricula) references matricula(codigo)
 );
 
+create table ra (
+	codigo char(6) primary key,
+    descripcion text,
+    mod_prof char(6),
+    constraint fk_re_modulo foreign key (mod_prof) references modulo_prof(codigo)
+);
+
 create table tarea (
 	codigo char(6) primary key,
     nombre varchar(80),
     fecha date,
     alumno char(9),
-    constraint  fk_tarea_alumno foreign key (alumno) references alumno(dni_alumno)
+    constraint fk_tarea_alumno foreign key (alumno) references alumno(dni_alumno)
+);
+
+create table realiza (
+	dni_alumno char(9),
+    codigo_tarea char(6),
+    nota int,
+    constraint chk_nota check (nota > 0 and nota < 10),
+    constraint pk_realiza primary key (dni_alumno, codigo_tarea, nota),
+    constraint fk_realiza_alumno foreign key (dni_alumno) references alumno(dni_alumno),
+	constraint fk_realiza_tarea foreign key (codigo_tarea) references tarea(codigo)
+);
+
+create table criterio_evalua (
+	codigo char(6) primary key,
+    nombre varchar(80),
+    descripcion text
+);
+
+create table se_evalua (
+	codigo_tarea char(6),
+    codigo_criterio char(6),
+    constraint pk_se_evalua primary key (codigo_tarea, codigo_criterio),
+    constraint fk_evalua_tarea foreign key (codigo_tarea) references tarea(codigo),
+    constraint fk_evalua_criterio foreign key (codigo_criterio) references criterio_evalua(codigo)
+);
+
+create table instrumento_evaluacion (
+	codigo char(6) primary key,
+    descripcion text,
+    tipo varchar(20),
+    tarea char(6),
+    constraint chk_intrumento check (tipo in ('Practica','Proyecto','Examen')),
+    constraint fk_instrumento_tarea foreign key (tarea) references tarea(codigo)
 );
